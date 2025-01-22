07d46d98debfc87fb509c6a749dd4591115f816c
Aret Camgöz <aretcamgoz@outlook.com>
2025-01-22 12:10:02 +0300

    dist update

---
+++ dist/constants.d.ts
@@ -0,0 +1,13 @@
+import { Percent } from '@uniswap/sdk-core';
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x32088851A85ED4900596B5DBea3Ae5Fc3Ca86De5";
+export declare const INIT_CODE_HASH = "0x376f5996e751ce66dbdcc44e8e26ee9e06dc64d921b6e255ca22795e25593ee8";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare const BASIS_POINTS: JSBI;
+export declare const ZERO_PERCENT: Percent;
+export declare const ONE_HUNDRED_PERCENT: Percent;
