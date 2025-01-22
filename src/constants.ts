7bc3b7250a986c83bafe3d8e95ea8e494c02f7a9
Aret Camgöz <aretcamgoz@outlook.com>
2025-01-22 11:30:24 +0300

    update constants for test

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import { Percent } from '@uniswap/sdk-core'
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x32088851A85ED4900596B5DBea3Ae5Fc3Ca86De5'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x376f5996e751ce66dbdcc44e8e26ee9e06dc64d921b6e255ca22795e25593ee8'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
